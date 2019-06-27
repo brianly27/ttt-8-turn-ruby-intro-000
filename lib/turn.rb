d5949ced6a64439183b3ffbc44af7710f5e4cf54
Brian Ly <brian.kyang.ly@gmail.com>
2019-06-27 20:00:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,9 +36,6 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 
-  input = gets.strip
-  index = input_to_index(input)
-
   until valid_move?(board, index) == true
     puts "Please enter a valid move"
     input = gets.strip
